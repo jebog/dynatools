b46240eff79735d3fee44325d535dd8676e8a6e6
Jean-Marc Amon <bogninin@gmail.com>
2023-12-10 19:24:46 +0100

    Template OK Login OK Logout OK

--- ivelo/urls.py
+++ ivelo/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="ivelo.index"),
+    path("batches/", views.batches, name="ivelo.batches"),
+    path("jobs/", views.jobs, name="ivelo.jobs"),
+    path("new-batch/", views.create_batch, name="ivelo.new_batch"),
+    path("report/", views.report, name="ivelo.report"),
+    path("waves/", views.report, name="ivelo.waves"),
 ]
--- ivelo/views.py
+++ ivelo/views.py
@@ -1,10 +1,32 @@
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
 from django.shortcuts import render
 
-
-# Create your views here.
 
 @login_required
 def index(request):
     return render(request, "ivelo/index.html")
+
+
+@login_required
+def batches(request):
+    return render(request, "ivelo/jobs.html")
+
+
+@login_required
+def create_batch(request):
+    return render(request, "ivelo/create-batch.html")
+
+
+@login_required
+def jobs(request):
+    return render(request, "ivelo/jobs.html")
+
+
+@login_required
+def report(request):
+    return render(request, "ivelo/jobs.html")
+
+
+@login_required
+def waves(request):
+    return render(request, "ivelo/waves.html")
